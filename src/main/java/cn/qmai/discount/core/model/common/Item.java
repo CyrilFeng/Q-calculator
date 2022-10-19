e624091e251b8c9e2f298f09f2b19eb21cb2f685
郭从文 <1491087363@qq.com>
2022-10-19 12:44:37 +0800

    Update Item.java

--- src/main/java/cn/qmai/discount/core/model/common/Item.java
+++ src/main/java/cn/qmai/discount/core/model/common/Item.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Shiyafeng
+ * Copyright 2022 CyrilFeng
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import java.io.Serializable;
 
 /**
  * 共享互斥关系中的元素
- * @author: shiyafeng
+ * @author: CyrilFeng
  * @date: 2022/8
  */
 @AllArgsConstructor
